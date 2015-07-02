7fbde61c0370324fbd79668b24163530025bbdec
Jennifer Fouquier <jennietf@gmail.com>
2015-07-02 14:16:46 -0700

    functioning website with css+html

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,7 @@
 import datetime
-
-
 from django.db import models
 from django.utils import timezone
 
-from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,7 +9,9 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date > timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
